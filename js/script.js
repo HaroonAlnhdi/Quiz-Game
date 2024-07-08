003f8cb4d8673b957c8b4e84d030607546c8fa20
Haroon <you@example.com>
2024-07-08 09:25:57 +0300

    change css in message container

--- js/script.js
+++ js/script.js
@@ -6,16 +6,18 @@
  const startBtn = document.querySelector('.start-button');
  const body = document.querySelector('body');
  const questionsContainer = document.querySelector('.questions-container');
+ const messeageContainer = document.querySelector('.messsage-container')
 //  console.log(startBtn);
 //  console.log(body)
 
 /*-------------------------------- Functions --------------------------------*/
     const start = () => {
-        console.log('start')
+        // console.log('start')
        let startcont = document.querySelector('.start-container');
-      
+        
        startcont.hidden = true ;
        questionsContainer.hidden = false ;
+       messeageContainer.hidden = false
     }
 
 
